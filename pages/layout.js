5ed81568e9fc601d80228eda9345b84d22b89ac0
Jon Sundin <sundinjon@gmail.com>
2023-08-16 02:26:28 -0700

    added context provider to layout

--- pages/layout.js
+++ pages/layout.js
@@ -1,13 +1,16 @@
 import { Fragment } from "react";
 import MainNav from "@/components/MainNav";
 import Footer from "@/components/Footer";
+import GraphDataProvider from "@/context/graph-data-context";
 
 export default function RootLayout({ children }) {
   return (
-    <Fragment>
+    <GraphDataProvider>
       <MainNav />
-      <main className="h-screen relative -mt-[150px] pt-[150px]">{children}</main>
+      <main className="h-screen relative z-1 -mt-[150px] pt-[150px]">
+        {children}
+      </main>
       <Footer />
-    </Fragment>
+    </GraphDataProvider>
   );
 }
