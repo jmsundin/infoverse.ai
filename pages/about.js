3842d59f150e52c0ce3700397dac8f55d5c3be74
Jon Sundin <sundinjon@gmail.com>
2023-08-16 02:22:16 -0700

    added basic about page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+"use client";
+
+function AboutPage(){
+    return(
+        <div className="text-white p-5 flex flex-col items-center">
+            <h1 className="text-xl">About Infoverse AI</h1>
+            <p className="w-2/3">  Infoverse AI is a web application that allows users to search for information in a visual network graph. The application uses Wikidata.org and Wikipedia.org.</p>
+        </div>
+    )
+}
+
+export default AboutPage;
