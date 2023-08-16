a3865f15937fbdd3df9f605d99088a5e836e8bb0
Jon Sundin <sundinjon@gmail.com>
2023-08-16 02:21:51 -0700

    moved fetching to context provider

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,24 @@
 "use client";
 
-import { Fragment, useState, useEffect, useRef } from "react";
-import * as d3 from "d3";
+import { Fragment, useState, useRef, useContext } from "react";
+import { GraphDataContext } from "@/context/graph-data-context";
+
 import SplitPane from "@/components/SplitPane";
 import { IoClose } from "react-icons/io5";
 
 import { Progress } from "@/components/ui/progress";
-import SPARQLQueryDispatcher from "@/lib/SPARQLQueryDispatcher";
-import { endpointUrl, sparqlQuery } from "@/data/sparqlQueryParams";
+
 import Graph from "@/components/Graph";
-import { parseWikidata } from "@/lib/utils";
 
 function HomePage() {
+  const { nodeQID, setNodeQID, graphData, setGraphData } = useContext(GraphDataContext);
   const [progress, setProgress] = useState(0);
   const [fetchingData, setFetchingData] = useState(false);
   const [pageIsLoading, setPageIsLoading] = useState(true);
   const [wikipediaPageUrl, setWikipediaPageUrl] = useState(null);
   const [splitPaneView, setSplitPaneView] = useState(false);
 
-  const [graphData, setGraphData] = useState(null);
-
   const graphContainer = useRef();
-
-  useEffect(() => {
-    async function getGraphData() {
-      const queryDispatcher = new SPARQLQueryDispatcher(endpointUrl);
-      const response = await queryDispatcher.query(sparqlQuery);
-      const graphData = await parseWikidata(response);
-      setGraphData(graphData);
-    }
-    getGraphData();
-  }, []);
 
   function handleSplitPaneViewClose() {
     setSplitPaneView(false);
@@ -44,17 +32,12 @@ function HomePage() {
 
   return (
     <Fragment>
-      {graphData !== null && !splitPaneView && (
+      {!splitPaneView && (
         <div
           className="flex flex-1 h-full w-full pl-2 pr-2"
           ref={graphContainer}
         >
-          {graphData !== null && (
-            <Graph
-              data={graphData}
-              handleWikipediaPageLoad={handleWikipediaPageLoad}
-            />
-          )}
+          <Graph handleWikipediaPageLoad={handleWikipediaPageLoad} />
         </div>
       )}
       {splitPaneView && (
@@ -67,13 +50,10 @@ function HomePage() {
             className="flex flex-1 h-full w-full pl-2 pr-2"
             ref={graphContainer}
           >
-            {graphData !== null && (
-              <Graph
-                data={graphData}
-                graphContainer={graphContainer}
-                handleWikipediaPageLoad={handleWikipediaPageLoad}
-              />
-            )}
+            <Graph
+              graphContainer={graphContainer.current}
+              handleWikipediaPageLoad={handleWikipediaPageLoad}
+            />
           </div>
 
           {/* <div className="flex flex-1 h-full border-2 rounded-md min-w-[100px] justify-center">
