861ce824e9908f2e63d16bed860d572f1c9f8835
Jon Sundin <sundinjon@gmail.com>
2023-08-16 02:22:27 -0700

    contact page start

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+"use client";
+
+function ContactPage(){
+    return(
+        <div>
+            <h1 className="flex flex-col items-center text-xl text-white">Contact Page</h1>
+        </div>
+    )
+}
+
+export default ContactPage;
