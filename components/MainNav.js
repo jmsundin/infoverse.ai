372a14e3f068bfc4871467088959638d93d3e8dc
Jon Sundin <sundinjon@gmail.com>
2023-08-16 21:40:36 -0700

    removed unused imports

--- components/MainNav.js
+++ components/MainNav.js
@@ -8,11 +8,9 @@ import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
@@ -39,57 +37,59 @@ export default function MainNav() {
   };
 
   return (
-    <div className="z-20 flex flex-row justify-between items-center gap-3 p-2">
+    <div className="z-20 flex flex-row justify-between items-center gap-3 p-2 bg-inherit">
       <Link
         href="/"
         className="flex flex-initial items-start p-3 hover:cursor-pointer hover:text-indigo-200 text-3xl text-white font-bold hover:text-indigo-200 r tracking-tight lg:text-4xl z-20"
       >
         Infoverse AI
       </Link>
-      <div className="flex flex-1 justify-center">
+      <div className="flex flex-1 justify-center mx-auto max-w-[600px] bg-inherit">
         <QueryForm />
       </div>
-      <NavigationMenu className="hidden z-20 sm:inline-block sm:mx-auto sm:my-2">
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/about" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                About
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/contact" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Contact
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <div className="hidden md:flex md:flex-initial md:justify-end md:items-center md:gap-3 md:z-20 md:mx-auto md:my-2">
+        <NavigationMenu className="z-20">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Home
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/about" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  About
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/contact" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Contact
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
 
       {/* Mobile Menu */}
       {!isMenuOpen && (
         <RiMenu3Line
-          className="z-20 relative text-white text-3xl cursor-pointer hover:fill-indigo-200 sm:hidden"
+          className="z-20 relative text-white text-3xl cursor-pointer hover:fill-indigo-200 md:hidden"
           onClick={showModalMenu}
         />
       )}
       {isMenuOpen && (
         <div
           ref={modalRef}
-          className="z-20 absolute top-0 right-0 flex flex-col w-full h-full p-2 bg-gradient-to-r from-indigo-950 to-indigo-500 sm:hidden"
+          className="z-20 absolute top-0 right-0 flex flex-col w-full h-full p-2 bg-gradient-to-r from-indigo-950 to-indigo-500 md:hidden"
         >
           <div className="flex flex-row justify-end">
             <RiCloseLine
-              className="flex flex-row text-white text-4xl cursor-pointer hover:fill-indigo-200 sm:hidden"
+              className="flex flex-row text-white text-4xl cursor-pointer hover:fill-indigo-200 md:hidden"
               onClick={showModalMenu}
             />
           </div>
