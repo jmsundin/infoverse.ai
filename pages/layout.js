f6bcdfb3ba7fac49546c0bd7a423b7089ab0e610
Jon Sundin <sundinjon@gmail.com>
2023-08-16 21:38:59 -0700

    removed z index

--- pages/layout.js
+++ pages/layout.js
@@ -1,13 +1,13 @@
-import { Fragment } from "react";
 import MainNav from "@/components/MainNav";
 import Footer from "@/components/Footer";
 import GraphDataProvider from "@/context/graph-data-context";
+import { Fragment } from "react";
 
 export default function RootLayout({ children }) {
   return (
     <GraphDataProvider>
       <MainNav />
-      <main className="h-screen relative z-1 -mt-[150px] pt-[150px]">
+      <main className="h-screen relative -mt-[150px] pt-[150px]">
         {children}
       </main>
       <Footer />
