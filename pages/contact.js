afe0cf551d6e3f918e9ca3a445d07b4562631ebf
Jon Sundin <sundinjon@gmail.com>
2023-08-20 21:09:22 -0700

    contact and about pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,18 @@
 "use client";
 
-function ContactPage(){
-    return(
-        <div>
-            <h1 className="flex flex-col items-center text-xl text-white">Contact Page</h1>
-        </div>
-    )
+import Head from "next/head";
+
+function ContactPage() {
+  return (
+    <div>
+      <Head>
+        <title>Infoverse AI | Contact</title>
+      </Head>
+      <h1 className="flex flex-col items-center text-xl text-white">
+        Contact Page
+      </h1>
+    </div>
+  );
 }
 
 export default ContactPage;
